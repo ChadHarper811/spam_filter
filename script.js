74ecac7a4ae3685f11273f239207589977161ad5
Chad Harper <chadharper811@gmail.com>
2024-06-16 10:31:37 -0500

    added event handler with an alert for emtpy message

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
+
+checkMessageButton.addEventListener("click", () => {
+    if(messageInput.value === "") {
+        alert("Please enter a message.");
+        return;
+    }
+});
