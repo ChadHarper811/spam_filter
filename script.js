7f83f2b8ea6d1fbafa4f70c171a25c59fcaf426a
Chad Harper <chadharper811@gmail.com>
2024-06-16 11:15:17 -0500

    added new regex for "free money" spam strings

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /(?:\s|^)fr[e3][e3] m[o0]n[e3]y(?:\s|$)/i;
 
-const denyList = [helpRegex, dollarRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
