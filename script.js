a9a284a8019eae4357d6830358ab8ef2371d082e
Chad Harper <chadharper811@gmail.com>
2024-06-16 10:17:10 -0500

    added starting variables to js

---
+++ script.js
@@ -0,0 +1,3 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
