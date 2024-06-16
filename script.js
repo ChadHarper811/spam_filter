6e3230618ef010ebd902e5abd7b986edefb0f78f
Chad Harper <chadharper811@gmail.com>
2024-06-16 10:48:10 -0500

    added a regex for testing spam and function for updating content after testing

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
+const helpRegex = /please help|assist me/i;
+
+const isSpam = (msg) => helpRegex.test(msg);
+
 checkMessageButton.addEventListener("click", () => {
     if(messageInput.value === "") {
         alert("Please enter a message.");
         return;
     }
+    result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message." : "This message does not seem to contain any spam.";
+    messageInput.value = "";
 });
