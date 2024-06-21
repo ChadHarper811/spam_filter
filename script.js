85a7f61359276543f2ddb528741748df6d69f956
Chad Harper <chadharper811@gmail.com>
2024-06-21 16:20:45 -0500

    added new regex

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 const freeRegex = /(?:\s|^)fr[e3][e3] m[o0]n[e3]y(?:\s|$)/i;
+const stockRegex = /stock alert/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
